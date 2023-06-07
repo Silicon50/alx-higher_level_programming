333ef5c5359fc109b0f2d0b2b974c31cc2423a43
Silicon50 <ndukwechukwuaja@gmail.com>
2023-06-07 06:06:03 +0100

    print alternating alphabets

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
 
 for alp in reversed(range(97, 123)):
-    print("{:c}".format(alp if (alp % 2 == 0) else (alp - 32)), end='')
+	if alp % 2 == 0:
+		print("{:c}".format(alp), end="")
+	else:
+		print("{:c}".format(alp-32), end ="")

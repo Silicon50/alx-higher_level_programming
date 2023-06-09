01d6f79c01b3400692a698fcaabcdf24351ce123
Silicon50 <ndukwechukwuaja@gmail.com>
2023-06-09 05:36:16 +0100

    argument length

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -3,14 +3,14 @@
 if __name__ == "__main__":
     from sys import argv
 
-    n = len(argv)
-    arg = n - 1
+    arg_len = len(argv)
+    real_len = arg_len - 1
 
-    if arg == 0:
+    if real_len == 0:
         print("0 arguments.")
-    elif arg == 1:
+    elif real_len == 1:
         print("1 argument:\n1: {:s}".format(argv[1]))
     else:
-        print("{:d} arguments:".format(arg))
-        for i in range(1, n):
+        print("{:d} arguments:".format(real_len))
+        for i in range(1, arg_len):
             print("{:d}: {:s}".format(i, argv[i]))

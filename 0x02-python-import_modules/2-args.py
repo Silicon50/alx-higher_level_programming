4ff1721d9ce946563adecae2ef46c10873033b4c
Silicon50 <ndukwechukwuaja@gmail.com>
2023-06-09 04:09:28 +0100

    print no of arguments

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,16 +1,16 @@
 #!/usr/bin/python3
 
-from sys import argv
-
 if __name__ == "__main__":
-	n = len(argv)
-	arg = n - 1
+    import sys
 
-	if arg == 0:
-		print("0 arguments.")
-	elif arg == 1:
-		print("1 argument:\n1: {:s}".format(argv[1]))
-	else:
-		print("{:d} arguments:".format(arg))
-	for i in range(1, n):
-		print("{:d}: {:s}".format(i, argv[i]))
+    n = len(sys.argv)
+    arg = n - 1
+
+    if arg == 0:
+        print("0 arguments.")
+    elif arg == 1:
+        print("1 argument:\n1: {:s}".format(sys.argv[1]))
+    else:
+        print("{:d} arguments:".format(arg))
+        for i in range(1, n):
+            print("{:d}: {:s}".format(i, sys.argv[i]))

37f887ede6f564905fe9de180625fabb72ad55fc
Silicon50 <ndukwechukwuaja@gmail.com>
2023-06-09 02:34:53 +0100

    add two numbers

---
+++ 0x02-python-import_modules/0-add.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+if __name__ == "__main__":
+	from add_0 import add
+
+	"""Define my arguments
+
+		Args:
+		a = 1
+		b = 2
+
+		Return Value = 3
+	"""
+	a = 1
+	b = 2
+	print("{} + {} = {}".format(a, b, add(a,b)))
+

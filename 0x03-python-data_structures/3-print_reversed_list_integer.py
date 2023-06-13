9edc6fb4e171c805b05fa8b2a8cb7db7f63b956e
Silicon50 <ndukwechukwuaja@gmail.com>
2023-06-13 03:30:53 +0100

    print element in a list in revered order

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -2,5 +2,6 @@
 
 def print_reversed_list_integer(my_list=[]):
     if my_list:
-        for element in reversed(my_list):
+        my_list = reversed(my_list)
+        for element in my_list:
             print("{:d}".format(element))

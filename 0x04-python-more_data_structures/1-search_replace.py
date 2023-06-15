4838bde575d070f4379b1f0b1b2c8a5eb88c17d2
Silicon50 <ndukwechukwuaja@gmail.com>
2023-06-15 05:02:25 +0100

    a function that returns a set of all elements present in only one set.

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 
-def search_replace(my_list, search, replace):chmo
+def search_replace(my_list, search, replace):
     sec_list = my_list.copy()
 
     for i in range(0, len(sec_list)):
--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 def uniq_add(my_list=[]):
-    
     num = 0
     for i in set(my_list):
         num += i

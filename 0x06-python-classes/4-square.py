90208bf8ee1ce0ef975379bb550cae6067569840
Silicon50 <ndukwechukwuaja@gmail.com>
2023-06-28 05:52:11 +0100

    get area of class

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -6,14 +6,12 @@ class Square:
     """Represent a square."""
     def __init__(self, size=0):
         """Initialize a new square.
-        Args:
-            size (int): The size of the new square.
         """
         self.size = size
 
     @property
     def size(self):
-        """Get the current size of the square."""
+        """Get size."""
         return (self.__size)
 
     @size.setter
@@ -26,5 +24,5 @@ class Square:
         self.__size = value
 
     def area(self):
-        """Return the current area of the square."""
+        """Return area"""
         return self.__size ** 2

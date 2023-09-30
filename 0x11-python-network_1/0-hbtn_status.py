07c123afd44b233ca01302faa64e4de3a35fbc5c
Silicon50 <ndukwechukwuaja@gmail.com>
2023-09-30 05:18:09 +0100

    get http request

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,15 +1,15 @@
 #!/usr/bin/python3
-"""  fetches https://alx-intranet.hbtn.io/status """
+"""get https://alx-intranet.hbtn.io/status """
 
 if __name__ == "__main__":
     from urllib.request import urlopen, Request
 
     url = "https://alx-intranet.hbtn.io/status"
     req = Request(url)
-    with urlopen(req) as res:
-        content = res.read()
+    with urlopen(req) as respond:
+        body = respond.read()
 
     print("Body response:")
-    print(f"\t- type: {type(content)}")
-    print(f"\t- content: {content}")
-    print(f"\t- utf8 content: {content.decode('utf-8')}")
+    print(f"\t- type: {type(body)}")
+    print(f"\t- content: {body}")
+    print(f"\t- utf8 content: {body.decode('utf-8')}")

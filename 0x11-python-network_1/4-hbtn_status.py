7ce2dc91fc27069c895f34743264103a28ed9669
Silicon50 <ndukwechukwuaja@gmail.com>
2023-09-30 05:29:20 +0100

    fetches url

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
-""" script that fetches https://alx-intranet.hbtn.io/status """
+""" fetches https://alx-intranet.hbtn.io/status """
 from requests import get
 
 if __name__ == '__main__':
-    res = get('https://alx-intranet.hbtn.io/status')
+    respond = get('https://alx-intranet.hbtn.io/status')
     print('Body response:')
-    print('\t- type:', type(res.text))
-    print('\t- content:', res.text)
+    print('\t- type:', type(respond.text))
+    print('\t- content:', respond.text)

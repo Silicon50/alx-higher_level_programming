c50ad9ef76557a7b68ef94df0522ff683e4ed39a
Silicon50 <ndukwechukwuaja@gmail.com>
2023-09-30 05:31:31 +0100

    send request and display the value

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
-""" displays the value of the variable X-Request-Id in the response header """
+""" a script that
+    -send request to url
+    -displays the value of the variable X-Request-Id in the response header """
 from sys import argv
 from requests import get
 

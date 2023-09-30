5415b2d1883685979e9b73631d88b8d9a209f0c3
Silicon50 <ndukwechukwuaja@gmail.com>
2023-09-30 05:23:08 +0100

    takes url and email and post

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
-"""2-post_email.py"""
+"""takes url and email"""
 from sys import argv
 from urllib import parse
 from urllib.request import Request, urlopen
 
 if __name__ == '__main__':
-    data = parse.urlencode({'email': argv[2]}).encode('ascii')
-    request = Request(argv[1], data)
-    with urlopen(request) as res:
+    body = parse.urlencode({'email': argv[2]}).encode('ascii')
+    req = Request(argv[1], body)
+    with urlopen(req) as res:
         print(res.read().decode('utf-8'))

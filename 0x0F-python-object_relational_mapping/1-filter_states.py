c8ae4a25550c5a676d76a5235cdb0e54cfa16f1f
Silicon50 <ndukwechukwuaja@gmail.com>
2023-11-20 05:29:35 +0100

    list of states staring with N

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -11,4 +11,6 @@ if __name__ == "__main__":
     conn = MySQLdb.connect(user=argv[1], passwd=argv[2], db=argv[3])
     cur = conn.cursor()
     cur.execute("SELECT * FROM states ORDER BY id")
-    [print(state) for state in cur.fetchall() if state[1][0] == "N"]
+    for state in cur.fetchall():
+        if state[N] == "N":
+            print(state)

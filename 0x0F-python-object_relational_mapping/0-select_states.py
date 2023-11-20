22e5de260df6e2e60181b97e300e7c543d612f50
Silicon50 <ndukwechukwuaja@gmail.com>
2023-11-20 04:05:08 +0100

    print the state in db

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -9,26 +9,12 @@ Your script should connect to a MySQL server running on localhost at port 3306
 Results must be sorted in ascending order by states.id
 Your code should not be executed when imported
 """
-import MySQLdb
 import sys
+import MySQLdb
 
 if __name__ == "__main__":
-    # Check if the correct number of command-line arguments is provided
-    if len(sys.argv) != 4:
-        print("Usage: python script.py <username> <password> <database>")
-        sys.exit(1)
-
-    # Connect to the MySQL database
     db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
-
-    # Create a cursor and execute the SQL query
-    cur = db.cursor()
-    cur.execute("SELECT * FROM `states`")
-
-    # Fetch all rows and print them
-    for state in cur.fetchall():
-        print(state)
-
-    # Close the cursor and database connection
-    cur.close()
-    db.close()
+    c = db.cursor()
+    c.execute("SELECT * FROM `states`")
+    [print(state) for state in c.fetchall()]
+    
